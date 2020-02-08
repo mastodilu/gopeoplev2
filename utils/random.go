1fec12befc3c0a6a91cabf97f2f166b66f3acfbc
matteo di lucchio <matteo.dilucchio@edu.unito.it>
2020-02-08 16:08:10 +0100

    fix random generator

--- utils/random.go
+++ utils/random.go
@@ -5,17 +5,22 @@ import (
 	"time"
 )
 
-var s = rand.NewSource(time.Now().UnixNano())
-var r = rand.New(s)
-
 // NewRandomInt returns a pseudo random int
 func NewRandomInt() int {
-	return rand.Int()
+	time.Sleep(time.Millisecond * 3)
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	return r.Int()
 }
 
 // NewRandomIntInRange returns a pseudo random int
 // in the range [min, max]
 func NewRandomIntInRange(min, max int) int {
+	time.Sleep(time.Millisecond * 3)
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
 	out := r.Intn(max+1-min) + min
 	return out
 }
